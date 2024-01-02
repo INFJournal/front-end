2b9b9c331834eb9fa64602e9f8c5d1e78fff94ef
XinguOh <ojyoung2490@gmail.com>
2024-01-03 01:29:59 +0900

    ✨ Dev: "폰트 추가"
    폰트 추가
    
    Resolves: #3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "./assets/Font/font.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
