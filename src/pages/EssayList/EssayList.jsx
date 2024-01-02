68c5d38a8ac40392a04e49dea6e775ab3c145bbe
dldusgh318 <pololydotoly@gmail.com>
2024-01-02 20:50:20 +0900

    feat:에세이리스트좋아요기능까지구현(#2)

--- src/pages/EssayList/EssayList.jsx
+++ src/pages/EssayList/EssayList.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
+import { Essay,LikeButton } from "./EssayList.style";
+
+
 
 export default function EssayList() {
+  const data=[1,2,3,4,5]
+  //const [button,setButton]=useState('likebtn');
+  let [like,setlikebtn]=useState(Array(data.length).fill(0));
+
+  const likebtnOn=(index)=>{
+    let likeCnt=[...like];
+    likeCnt[index]++;
+    setlikebtn(likeCnt);
+  };
+
+
     return (
-      <Essay>
-      <h4>"오늘의 명언"</h4>
-      <Button>🥰</Button>
-      </Essay>
+      data.map(function(id,index){
+        return(
+          <Essay key={id}>
+              <h4 style={{ fontSize: "13px", margin: "0 0 0 0", textAlign: "left" }}>{`"오늘의 명언 ${id}"`}</h4>
+              <div style={{ position: "absolute", top: "0", right: "0",marginRight:"10px"}}>
+                <div style={{fontWeight:"bold"}}>{`❤️${like[index]}🔖`} </div>
+            </div>
+            <LikeButton onClick={() => likebtnOn(index)}>🥰</LikeButton>
+          </Essay>
+        );
+      })
+
     );
     
   }
