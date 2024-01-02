d1c6017847cb3554fe6d22373a6f07637b595a2f
nanna29 <luna2907@naver.com>
2024-01-02 16:56:06 +0900

    feat: Add FeelingChoice page (#1)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import GlobalStyle from "./components/Global.style.jsx";
 
 import Home from "./pages/Home";
+import FeelingChoice from "./pages/FeelingChoice";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/feelingchoice" element={<FeelingChoice />} />
         </Routes>
       </BrowserRouter>
     </>
