bdffa15a44f1b3fdb89f28afdd40b3b139762979
dldusgh318 <pololydotoly@gmail.com>
2024-01-03 04:27:29 +0900

    feat:에세이랜덤리스트api까지(#2)

--- src/pages/EssayRandomList/EssayRandomList.jsx
+++ src/pages/EssayRandomList/EssayRandomList.jsx
@@ -1,44 +1,55 @@
-import React,{useState} from "react";
-import {Essay, LikeButton,JbDivisionLine,ScrapImgs,LikeImgs} from "./EssayRandomList.style";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Essay,
+  LikeButton,
+  JbDivisionLine,
+  ScrapImgs,
+  LikeImgs
+} from "./EssayRandomList.style";
 import Header from "../../components/HeaderList/MainBackHeader";
 import UnScrapImg from "../../img/UnScrap.png";
 import ScrapImg from "../../img/Scrap.png";
 import UnlikeImg from "../../img/Heart.png";
 
 export default function EssayList() {
-    const data=[1,2,3,4,5]
-    let [scrap]=useState(Array(data.length).fill(0));
-    let [like]=useState(Array(data.length).fill(0));
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    // API 엔드포인트를 실제 엔드포인트로 교체
+    const apiEndpoint = `http://10.10.140.49:8080/api/me/inbox/essays`;
 
+    axios
+      .get(apiEndpoint, {
+        headers: {
+          Authorization: 4
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        setData(response.data.result);
+        // const count = response.data.result.length
+        // console.log(count);
+      })
+      .catch((error) => {
+        console.error("데이터 가져오기 오류:", error);
+      });
+  }, []);
 
-    return(
-        <div>
-            <Header />
-            {/* <ScrEs>
-                <TopicBox>그 날의 토픽</TopicBox>
-                <Topic>"그 날의 명언"</Topic>
-                    <div style={{ position: "absolute", left: "16%",top:"7cm",fontWeight:"bold"}}>
-                        {`🔖`} 
-                    </div>
-                    <div style={{  position: "absolute", right: "17%",top:"7.2cm", fontSize:"11px"}}>
-                        글쓴이 | ㅇㅇ
-                    </div>
-            </ScrEs> */}
-            <JbDivisionLine/>
-        {data.map(function(item,index){
-          return(
-            <Essay key={index}>
-                <h4 style={{ fontSize: "13px", margin: "-5px 0 0 -30px", textAlign: "left" }}>{`"그날의 명언 ${item}"`}</h4>
-                <div style={{ position: "absolute", top: "0", right: "0",marginRight:"10px"}}>
-
-                  <LikeImgs src={ like[index]? "❤️" :UnlikeImg} alt="like" />
-                  <ScrapImgs src={ scrap[index]? ScrapImg :UnScrapImg} alt="Scrap" />
-              </div>
-              <LikeButton>{item}</LikeButton>
-            </Essay>
-          );
-        })}
-      </div>
-    );
+  return (
+    <div>
+      <Header />
+      <JbDivisionLine />
+      {data.map((essay) => (
+        <Essay key={essay.essayId}>
+          <h4 style={{ fontSize: "13px", margin: "-5px 0 0 -30px", textAlign: "left" }}>{`"${essay.topic} ${essay.essayId}"`}</h4>
+          <div style={{ position: "absolute", top: "0", right: "0", marginRight: "10px" }}>
+            <LikeImgs src={essay.isLiked ? "❤️" : UnlikeImg} alt="like" />
+            <ScrapImgs src={essay.isScraped ? ScrapImg : UnScrapImg} alt="Scrap" />
+          </div>
+          <LikeButton>{essay.likeType}</LikeButton>
+        </Essay>
+      ))}
+    </div>
+  );
 }
